513614c75c13be789932de5e37aa3e056babbd20
sherlock-admin <95431921+sherlock-admin@users.noreply.github.com>
2023-12-04 19:10:40 +0100

    Delete scope directories

--- bophades/shell/full_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pnpm install
-git submodule init
-git submodule update
-forge install
-forge update
-forge build
--- bophades/shell/verify_kernel_installation.sh
+++
@@ -1,5 +0,0 @@
-# Load environment variables
-source .env
-
-# Verify modules and policies were correctly installed
-forge script ./src/scripts/deploy/DeployV2.sol:OlympusDeploy --sig "verifyKernelInstallation()()" --rpc-url $RPC_URL --slow -vvv
