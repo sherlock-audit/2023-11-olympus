1431d8f159982cbc471320a761e36bdab97f1b5b
sherlock-admin <95431921+sherlock-admin@users.noreply.github.com>
2023-12-04 15:56:10 +0100

    Delete scope directories

--- bophades/src/test/lib/zuniswapv2/interfaces/IZuniswapV2Factory.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.10;
-
-interface IZuniswapV2Factory {
-    function pairs(address, address) external pure returns (address);
-
-    function createPair(address, address) external returns (address);
-}
